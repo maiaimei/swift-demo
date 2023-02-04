faeb2bf8a9e8cf63eca8cd641e6dc54bffb7387c
maiaimei <1211674185@qq.com>
2023-02-04 20:40:49 +0800

    feat: MT798 validation

--- src/main/java/cn/maiaimei/framework/swift/config/SwiftMTConfig.java
+++ src/main/java/cn/maiaimei/framework/swift/config/SwiftMTConfig.java
@@ -1,10 +1,8 @@
 package cn.maiaimei.framework.swift.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties
 @Configuration
 @ComponentScan("cn.maiaimei.framework.swift.validation")
 public class SwiftMTConfig {
